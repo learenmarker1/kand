b08e31c7683e58efa6b49e3c71050c2ad59f8bcb
learenmarker1 <lea.renmarker@gmail.com>
2019-04-03 14:07:40 +0200

    diverse

--- testapp/urls.py
+++ testapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='test-homepage'),
+    path('about/', views.about, name='test-aboutpage'),
 ]
--- testapp/views.py
+++ testapp/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def home (request):
-    return HttpResponse('<h1>Mood managers are here</h1>')
+def home(request):
+    #return HttpResponse('<h1>We are the Mood managers</h1>')
+    return render(request, 'testapp/home.html')
+
+def about(request):
+    #return HttpResponse('<h1>More about our mood managers</h1>')
+    return render(request, 'testapp/about.html')
