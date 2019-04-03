5117ef104699456fd75defe01d07216f98e6eb07
HannaLycken <Hanna.Lycken.4861@student.uu.se>
2019-04-03 10:32:11 +0200

    testing

--- kand_project/settings.py
+++ kand_project/settings.py
@@ -10,6 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
+
+fdjkfjalkd
+
+
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
