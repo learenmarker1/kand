9ad68069ce50fa1723f9554f84cb911d6e80aaaa
HannaLycken <Hanna.Lycken.4861@student.uu.se>
2019-04-24 14:30:04 +0200

    urls och frågor

--- testapp/views.py
+++ testapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
-
+import random
 from .models import Choice, Question
 
 
@@ -11,8 +11,8 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """Return 3 random questions from the database."""
+        return Question.objects.order_by('?')[:3]
 
 
 class DetailView(generic.DetailView):
@@ -64,7 +64,7 @@ def answer1(request, question_id):
     next_question_ids = Question.objects.filter(id__gt=question_id).order_by('id').values('id')
     if next_question_ids:
         next_question_id = next_question_ids[0]['id']
-        return HttpResponseRedirect(reverse('testapp:question1', args=(next_question_id,)))
+        return HttpResponseRedirect(reverse('testapp:question2', args=(next_question_id,)))
     else:
         HttpResponseRedirect(reverse('testapp:result'))
 
@@ -73,7 +73,7 @@ def answer2(request, question_id):
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-        return render(request, 'testapp/detail.html',{
+        return render(request, 'testapp:question2.htlm',{
         'question': question,
         'error_message': "You didn't select a choice.",
         })
@@ -83,7 +83,7 @@ def answer2(request, question_id):
     next_question_ids = Question.objects.filter(id__gt=question_id).order_by('id').values('id')
     if next_question_ids:
         next_question_id = next_question_ids[0]['id']
-        return HttpResponseRedirect(reverse('testapp:question2', args=(next_question_id,)))
+        return HttpResponseRedirect(reverse('testapp:question3', args=(next_question_id,)))
     else:
         return HttpResponseRedirect(reverse('testapp:result'))
 
@@ -92,7 +92,7 @@ def answer3(request, question_id):
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-        return render(request, 'testapp/detail.html',{
+        return render(request, 'testapp:question3.htlm',{
         'question': question,
         'error_message': "You didn't select a choice.",
         })
