1d75d643c76d7d8acfa6e49b700af1dde406c35c
learenmarker1 <lea.renmarker@gmail.com>
2019-05-10 14:05:50 +0200

    authentication på alla sidor

--- testapp/views.py
+++ testapp/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
+from django.conf import settings
+from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
@@ -14,7 +13,7 @@ class IndexView(generic.ListView):
 
     def get_queryset(self, **kwargs):
         """Return 3 random questions from the database."""
-        return Poll.objects.get(pk=1).questions.all()
+        return Poll.objects.latest('pk').questions.all()
 
 
 class DetailView(generic.DetailView):
@@ -74,7 +73,7 @@ def answer1(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-    next_question_ids = Poll.objects.get(pk=1).questions.filter(id__gt=question_id).order_by('id').values('id')
+    next_question_ids = Poll.objects.latest('pk').questions.filter(id__gt=question_id).order_by('id').values('id')
     if next_question_ids:
         next_question_id = next_question_ids[0]['id']
         return HttpResponseRedirect(reverse('testapp:question2', args=(next_question_id,)))
@@ -93,7 +92,7 @@ def answer2(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-    next_question_ids = Poll.objects.get(pk=1).questions.filter(id__gt=question_id).order_by('id').values('id')
+    next_question_ids = Poll.objects.latest('pk').questions.filter(id__gt=question_id).order_by('id').values('id')
     if next_question_ids:
         next_question_id = next_question_ids[0]['id']
         return HttpResponseRedirect(reverse('testapp:question3', args=(next_question_id,)))
@@ -112,7 +111,7 @@ def answer3(request, question_id ):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-    next_question_ids = Poll.objects.get(pk=1).questions.filter(id__gt=question_id).order_by('id').values('id')
+    next_question_ids = Poll.objects.latest('pk').questions.filter(id__gt=question_id).order_by('id').values('id')
     if next_question_ids:
         next_question_id = next_question_ids[0]['id']
         return HttpResponseRedirect(reverse('testapp:result', args=(next_question_id,)))
